d9db4996a91e6e2750e48ddf7d3b448e607cfb93
aiko77777 <a21471957@yahoo.com>
2024-06-12 01:23:39 +0800

    test2

--- src/main/java/com/PD2/Tetris/block/Cell.java
+++ src/main/java/com/PD2/Tetris/block/Cell.java
@@ -1,4 +1,13 @@
 package com.PD2.Tetris.block;
+import java.awt.Image.*;
+import java.awt.image.BufferedImage;
 
 public class Cell {
+    public  int  row;
+    public  int column;
+    public BufferedImage cell_image;
+
+    public  Cell(int row,int column,BufferedImage cell_image){
+
+    }
 }
