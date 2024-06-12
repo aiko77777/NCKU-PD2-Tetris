91505da376df98c2d5b914fa53de7a8fdf00a629
Hell HBBD <paulyu602@gmail.com>
2024-06-12 17:05:42 +0800

    implement Tetromino.java

--- src/main/java/com/PD2/Tetris/block/Tetromino.java
+++ src/main/java/com/PD2/Tetris/block/Tetromino.java
@@ -1,4 +1,83 @@
 package com.PD2.Tetris.block;
 
-public class Tetromino {
+import com.PD2.Tetris.shape.*;
+import java.awt.image.BufferedImage;
+
+public abstract class Tetromino {
+	private Cell center;
+	private int rotateTime;
+	private State[] states;
+	private BufferedImage image;
+
+	public static Tetromino random() {
+		int random = (int) Math.random() * 7;
+		Tetromino t = null;
+		switch (random) {
+			case 0:
+				t = new I();
+				break;
+			case 1:
+				t = new J();
+				break;
+			case 2:
+				t = new L();
+				break;
+			case 3:
+				t = new O();
+				break;
+			case 4:
+				t = new S();
+				break;
+			case 5:
+				t = new Z();
+				break;
+			case 6:
+				t = new I();
+				break;
+		}
+		return t;
+	}
+
+	public Tetromino() {
+		rotateTime = 0;
+		center = new Cell(5, 0);
+		states = null;
+		image = null;
+	}
+
+	public Tetromino(int x, int y) {
+		rotateTime = 0;
+		center = new Cell(x, y);
+		states = null;
+		image = null;
+	}
+
+	public void moveLeft() {
+		center.moveLeft();
+	}
+
+	public void moveDown() {
+		center.moveDown();
+	}
+
+	public void moveRight() {
+		center.moveRight();
+	}
+
+	public void rotate() {
+		rotateTime++;
+	}
+
+	public abstract int[][] getBlockPositions();
+
+	class State {
+		Cell[] states;
+
+		State(int[][] positions) {
+			states = new Cell[4];
+			for (int i = 0;i < 4;i++) {
+				states[i] = new Cell(positions[i][0], positions[i][1]);
+			}
+		}
+	}
 }
