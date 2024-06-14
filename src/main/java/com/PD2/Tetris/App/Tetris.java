8e7476d17fb30a6b90aced20e95134033de1b13a
jo3xjp6eji <64538143+jo3xjp6eji@users.noreply.github.com>
2024-06-14 23:10:48 +0800

    Update Tetris.java

--- src/main/java/com/PD2/Tetris/App/Tetris.java
+++ src/main/java/com/PD2/Tetris/App/Tetris.java
@@ -1,12 +1,16 @@
 package com.PD2.Tetris.App;
 
-import com.PD2.Tetris.block.Cell;
-import com.PD2.Tetris.block.Tetromino;
+import com.PD2.Tetris.Game.GameController;
 
 import javax.swing.JPanel;
 
 public class Tetris extends JPanel {
-	public static void main(String[] args) {
-		new Menu();
-	}
+    public static void main(String[] args) {
+        new Menu();
+    }
+
+    public void startGame() {
+        GameController gameController = new GameController();
+        gameController.start();
+    }
 }
