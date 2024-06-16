c6e75448a3ecf2352e59b8363c3ca1def3c8baea
jo3xjp6eji <64538143+jo3xjp6eji@users.noreply.github.com>
2024-06-16 14:11:39 +0800

    Update Tetris.java

--- src/main/java/com/PD2/Tetris/App/Tetris.java
+++ src/main/java/com/PD2/Tetris/App/Tetris.java
@@ -1,16 +1,20 @@
 package com.PD2.Tetris.App;
 
 import com.PD2.Tetris.Game.GameController;
-
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class Tetris extends JPanel {
     public static void main(String[] args) {
-        new Menu();
-    }
-
-    public void startGame() {
-        GameController gameController = new GameController();
-        gameController.start();
+        Menu menu = new Menu();
+        menu.start_button.addActionListener(actionEvent -> {
+            System.out.println("game start");
+            menu.frame.dispose();
+            JFrame game_frame = new JFrame("NCKU Tetris");
+            GameController gameController = new GameController(game_frame);
+            game_frame.setSize(810, 940);
+            game_frame.add(gameController);
+            game_frame.setVisible(true);
+            gameController.start();
+        });
     }
 }
